3ea181dd39d71ff62493b72cad1bf8c51d92d16f
Jordan Broomfield <Jordan.Broomfield2004@yahoo.com>
2025-08-26 23:25:43 -0400

    Initial commit-Password Checker and Test

---
+++ PasswordStrengthLib.Tests/PasswordCheckerTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using PasswordStrengthLib;
+
+namespace PasswordStrengthLib.Tests
+{
+    public class PasswordCheckerTests
+    {
+        [Theory]
+        [InlineData("", "INELIGIBLE")]
+        [InlineData("abc", "WEAK")]
+        [InlineData("ABC", "WEAK")]
+        [InlineData("12345", "WEAK")]
+        [InlineData("!!!!", "WEAK")]
+        [InlineData("abc123", "MEDIUM")]
+        [InlineData("Abc123", "MEDIUM")]
+        [InlineData("Abc123!", "STRONG")]
+        public void CheckStrength_ReturnsExpected(string password, string expected)
+        {
+            var result = PasswordChecker.CheckStrength(password);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ PasswordStrengthLib/PasswordChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PasswordStrengthLib
+{
+    public class PasswordChecker
+    {
+        public static string CheckStrength(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return "INELIGIBLE";
+
+            int criteriaMet = 0;
+
+            if (Regex.IsMatch(password, "[A-Z]")) criteriaMet++;
+            if (Regex.IsMatch(password, "[a-z]")) criteriaMet++;
+            if (Regex.IsMatch(password, "[0-9]")) criteriaMet++;
+            if (Regex.IsMatch(password, "[^a-zA-Z0-9]")) criteriaMet++;
+
+            if (criteriaMet == 0) return "INELIGIBLE";
+            if (criteriaMet == 1) return "WEAK";
+            if (criteriaMet == 2 || criteriaMet == 3) return "MEDIUM";
+            if (criteriaMet == 4) return "STRONG";
+
+            return "INELIGIBLE";
+        }
+    }
+}
