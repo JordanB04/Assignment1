39d179ba5a9392b4687271105deeb55383522083
Jordan Broomfield <Jordan.Broomfield2004@yahoo.com>
2025-09-15 15:26:42 -0400

    Docs: Added XML documentaiton for GenerateV4 UUID function

--- PasswordStrengthLib/UuidUtils.cs
+++ PasswordStrengthLib/UuidUtils.cs
@@ -7,10 +7,15 @@ namespace PasswordStrengthLib
     /// </summary>
     public static class UuidUtils
     {
-        /// <summary>
+        // <summary>
         /// Generates a Version 4 UUID (random) per RFC 4122.
+        /// Uses <see cref="Guid.NewGuid"/> which sets the version nibble to '4'
+        /// and the variant nibble to one of {8,9,A,B}.
         /// </summary>
-        /// <returns>A string like xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx where y ∈ {8,9,a,b}.</returns>
+        /// <returns>
+        /// UUID string in canonical form: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
+        /// where y ∈ {8,9,a,b}.
+        /// </returns>
         public static string GenerateV4()
         {
             // Guid.NewGuid() produces a version 4 UUID in .NET
